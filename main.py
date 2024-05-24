a3d7a03931595f9742b12470770f65bd33e8ed42
Igoroots34 <igorgdf34@gmail.com>
2024-05-23 22:14:46 -0300

    new problens

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 import discord
 from discord.ext import commands
+from flask import Flask, jsonify
+import threading
 
 # Substitua pelo seu token
-TOKEN = '1243030011385085972'
+TOKEN = 'MTI0MzAzMDAxMTM4NTA4NTk3Mg.GTJvBn.POzBGlsLTaq204ftHchDocbRmgHq_a4J0T2omE'
+GUILD_ID = '840795725159464982'  # Substitua pelo ID do seu servidor
 
-# Inicialize o bot
+# Inicialize o bot com intenções apropriadas
 intents = discord.Intents.default()
 intents.members = True  # Habilite a permissão de membros
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Inicialize o Flask app
+app = Flask(__name__)
+
+@app.route('/server_members', methods=['GET'])
+def get_server_members():
+    guild = bot.get_guild(int(GUILD_ID))
+    members = guild.members
+    members_list = [{'name': member.name, 'id': member.id} for member in members]
+    print("Lista de membros:", members_list)  # Adiciona um print statement para verificar a lista de membros
+    return jsonify(members_list)
 
 @bot.event
 async def on_ready():
     print(f'Bot conectado como {bot.user}')
 
-@bot.command()
-async def list_members(ctx):
-    members = ctx.guild.members
-    for member in members:
-        name = member.name
-        avatar = member.avatar_url
-        await ctx.send(f'Nome: {name}, Avatar: {avatar}')
+def run_flask():
+    app.run(host='0.0.0.0', port=5000)
+
+# Executa o Flask em uma thread separada
+flask_thread = threading.Thread(target=run_flask)
+flask_thread.start()
 
 bot.run(TOKEN)
